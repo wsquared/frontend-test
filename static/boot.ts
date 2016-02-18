b9ab37ee6d997e0fd4b89192068479512854384d
Willwin Wang <willwin.wang@xero.com>
2016-02-18 20:49:13 +1100

    Changed to small letter when referencing app.

--- static/boot.ts
+++ static/boot.ts
@@ -3,7 +3,7 @@ require('bootstrap-webpack');
 
 import 'angular2/bundles/angular2-polyfills';
 import { bootstrap } from 'angular2/platform/browser';
-import { App } from './App';
+import { App } from './app';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { CounterService } from './services/counterService';
 import configureStore from './stores/configureStore';
